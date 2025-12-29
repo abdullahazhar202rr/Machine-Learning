45ce3d4cac66e9a24216f481a804164655872154
Abdullah Azhar <abdullahazhar202rr@gmail.com>
2025-12-29 21:12:46 +0500

    Add all ML projects with password-protected backup zips

--- Projects/BTC_Predictor/btc_predictor_enhanced_structure/main.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from src.api import PredictionAPI
-
-api_instance = PredictionAPI()
-app = api_instance.get_app()
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
